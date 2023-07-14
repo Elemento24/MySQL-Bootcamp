280e7fe01c83c3a3e045dfba38fa04f16a07fe27
Elemento <mittalvishesh021@gmail.com>
2023-07-14 22:20:56 +0530

    Added the code for Section 03

---
+++ (03) Creating Databases & Tables/CODE.sql
@@ -0,0 +1,54 @@
+-- To display the available databases 
+-- SHOW DATABASES;
+
+-- Creating a new database
+-- CREATE DATABASE soap_shop;
+
+-- Using an existing database
+-- USE soap_shop;
+
+-- Dropping a database
+-- DROP DATABASE soap_shop;
+
+-- Creating a table
+-- CREATE DATABASE pet_shop;
+-- USE pet_shop;
+
+-- CREATE TABLE cats (
+-- 	name VARCHAR(50),
+--     age INT
+-- );
+
+-- CREATE TABLE dogs (
+-- 	name VARCHAR(50),
+--     breed VARCHAR(50),
+--     age INT
+-- );
+
+-- Displaying the tables in the current database
+-- SHOW TABLES;
+
+-- Displaying the columns for a table
+-- SHOW COLUMNS FROM cats;
+
+-- Describing an existing table
+-- DESC cats;
+
+-- Dropping a table
+-- DROP TABLE dogs;
+
+-- ---------------------------------
+-- EXERCISE: Tables Basics Activity
+-- ---------------------------------
+
+-- CREATE DATABASE bakery;
+-- USE bakery;
+
+-- CREATE TABLE pastries (
+-- 	name VARCHAR(50),
+--     quantity int
+-- );
+
+-- DESC pastries;
+-- DROP TABLE pastries;
+-- SHOW TABLES;
